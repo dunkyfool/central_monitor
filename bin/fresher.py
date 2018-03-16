10dd3bc6c193c74eddf81e88ad10826685223f69
Jackie <jackie@ghtinc.com>
2018-03-16 08:56:53 +0800

    new fresher for prometheus

--- bin/fresher.py
+++ bin/fresher.py
@@ -4,30 +4,30 @@
 # Author: Jackie                                  #
 ###################################################
 import os
-import pprint as pp
+from pprint import pprint
 import subprocess as sp
 
 #######################################
 # Load current node & bidagent status #
 #######################################
-pwd        = os.environ.get('PASSWORD') if('PASSWORD' in os.environ) else 'ryan'
 configFile = os.environ.get('CONFIG_FILE') if('CONFIG_FILE' in os.environ) else '/opt/central_monitor/bin/prometheus.yml'
 hostsFile  = os.environ.get('HOSTS_FILE') if('HOSTS_FILE' in os.environ) else '/etc/hosts'
-print '[info] pwd set, prefix == ' + prefix + ' configFile == ' + configFile + ' hostsFile == ' + hostsFile
 ctr = 0           # ctr is flag for checking whether process runs first time (0:Y)
-oldDict = {}      # last time bidagent config
+oldlist = []      # last time host in /etc/hosts
 wrFlag = 0        # rewrite nginx config
 
 def validateIP(s):
     splits = s.split('.')
-    if splits[0] != '10':
-        return False
-    if not len(splits) == 4:
-        return False
-    for numStr in splits:
-        num = int(numStr)
-        if num > 255 or num < 0:
+    try:
+        if not len(splits) == 4:
             return False
+        for numStr in splits:
+            num = int(numStr)
+            if num > 255 or num < 0:
+                return False
+    except:
+        print("Error! Due to "+s)
+        return False
     return True
 
 def takeLast(hostlist):
@@ -42,16 +42,14 @@ def takeLast(hostlist):
     for ip in ip2host:
         tmpHostList.append([ip2host[ip], ip])
     return tmpHostList
-    
+
 
 while True:
     ################
-    # Get Nodelist #
+    # Get hostlist #
     ################
-    nodeDict = {}               # name of nodes w/ bidagent
     hostlist = []               # for mapping hostname to ip
-    totalnodeAgentNum = 0       # Num of all agents
-    nodeNum = 0                 # Num of all nodes  w/ bidagent
+    nodeNum = 0                 # Num of all nodes
 
     with open(hostsFile, 'r') as f:
         # hostlist = [
@@ -67,30 +65,43 @@ while True:
         hostlist = takeLast(hostlist)
         print '[info] hostlist ==', hostlist
 
+        nodeNum = len(hostlist)
 
-    print '[info] nodeDict ==', nodeDict
-    nodeNum = len(nodeDict)
 
     ####################
     # Check Difference #
     ####################
-    if ctr==0:  # At beginning
-        oldDict = nodeDict
+    if ctr == 0:  # At beginning
+        oldlist = hostlist
         wrFlag=1
-    elif ctr==1:
+    elif ctr == 1:
         # seems like dictionary comparsison is guaranteed (recursively)
         # ref: http://stackoverflow.com/questions/1911273/is-there-a-better-way-to-compare-dictionary-values
-        if oldDict == nodeDict:
+        if oldlist == hostlist:
+            print '[info] /etc/hosts is the same'
             wrFlag=0
         else:
+            print '[info] /etc/hosts is different from last time'
             wrFlag=1
-            oldDict = nodeDict# change oldDict
+            oldlist = hostlist# change oldlist
+
+    ##################################
+    # Create string of listened host #
+    ##################################
+    str_hostlist = '\''
+    if nodeNum > 0:
+        for pair in hostlist:
+            str_hostlist += pair[1]+':9100\','
+        str_hostlist = str_hostlist[:-1]
+    else:
+        str_hostlist = '\'localhost:9100\''
 
     #################
     # Create config #
     #################
     if wrFlag==1:
         with open(configFile, 'w') as f:
+        #with open('tmpCFG', 'w') as f:
             f.write('global:\n')
             f.write('  scrape_interval:     15s\n')
             f.write('  evaluation_interval: 15s\n')
@@ -101,30 +112,22 @@ while True:
             f.write('scrape_configs:\n')
             f.write('  - job_name: \'node-exporter\'\n')
             f.write('    static_configs:\n')
-            f.write('            - targets: [\'localhost:9100\']\n\n')
+            f.write('            - targets: ['+str_hostlist+']\n\n')
             f.write('  - job_name: \'prometheus\'\n')
             f.write('    static_configs:\n')
             f.write('            - targets: [\'localhost:9090\']\n')
 
-            for host in nodeDict:
-                if len(nodeDict[host]['ports']) > 0:
-                    ip = nodeDict[host]['Address']
-                    f.write('  location /w/'+ip+'/ {\n')
-
-        ################
-        # Reload Nginx #
-        ################
+        #####################
+        # Reload Prometheus #
+        #####################
         if ctr==0:
-            cmd = '/usr/sbin/nginx -t && sleep 1'
-            os.system(cmd)
-            cmd = 'systemctl reload nginx'
+            print '[info] First time to restart prometheus'
+            cmd = 'systemctl restart prometheus'
             os.system(cmd)
             ctr=1
         else:
-            cmd = '/usr/sbin/nginx -t && sleep 1'
-            os.system(cmd)
-            #cmd = '/usr/sbin/nginx -s reload'
-            cmd = 'systemctl reload nginx'
+            print '[info] Found some difference in /etc/hosts and try to restart prometheus'
+            cmd = 'systemctl restart nginx'
             os.system(cmd)
 
     cmd = 'sleep 10'
