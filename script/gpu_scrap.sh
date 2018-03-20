6d2100aa31a30c391f5ff59df5b4f4ad07e2f1ee
Jackie <jackie@ghtinc.com>
2018-03-21 00:14:30 +0800

    gpu parser

--- script/gpu_scrap.sh
+++ script/gpu_scrap.sh
@@ -2,30 +2,32 @@
 
 # Declare
 FILE=test/log
+LOG=txt/gpu_metrics.prom
 TIME=`head -n1 ${FILE}`
 VERSION=`head -n3 ${FILE} | tail -n1 | cut -d' ' -f3`
 
 # Parser
 function gpu_parser {
+	rm -f ${LOG}
 	for LINE in 9 12 15 18 21 24 27 30
 	do
 		GPU=`head -n${LINE}  ${FILE} | tail -n2`
 		GPU_NO=`echo ${GPU} | cut -d'|' -f2 | cut -d' ' -f2`
-		GPU_FAN=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f2`
-		GPU_TEMP=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f3`
-		GPU_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f5`
-		GPU_MAX_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f7`
-		GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2`
-		GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4`
-		GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2`
+		GPU_FAN=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f2 | sed 's/.$//'`
+		GPU_TEMP=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f3 | sed 's/.$//'`
+		GPU_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f5 | sed 's/.$//'`
+		GPU_MAX_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f7 | sed 's/.$//'`
+		GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2 | sed 's/...$//'`
+		GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4 | sed 's/...$//'`
+		GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2 | sed 's/.$//'`
 		
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_FAN\"} ${GPU_FAN}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_TEMP\"} ${GPU_TEMP}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_MEM\"} ${GPU_MEM}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_MAX_MEM\"} ${GPU_MAX_MEM}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_PWR\"} ${GPU_PWR}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_MAX_PWR\"} ${GPU_MAX_PWR}"
-		echo "node_gpu_metrics{instance=${GPU_NO}, content=\"GPU_UTIL\"} ${GPU_UTIL}"
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_FAN\"} ${GPU_FAN}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_TEMP\"} ${GPU_TEMP}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MEM\"} ${GPU_MEM}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_MEM\"} ${GPU_MAX_MEM}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_PWR\"} ${GPU_PWR}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_PWR\"} ${GPU_MAX_PWR}" >> ${LOG}
+		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_UTIL\"} ${GPU_UTIL}" >> ${LOG}
 	done
 }
 
