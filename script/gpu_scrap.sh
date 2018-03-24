3f343e07e8fe1e28d51594fa16dfa3ff910b5b21
ryan <dunkyfool0113@gmail.com>
2018-03-25 00:11:49 +0800

    new gpu_scrap.sh to detect gpu separatelt with STATUS

--- script/gpu_scrap.sh
+++ script/gpu_scrap.sh
@@ -4,36 +4,53 @@
 FILE=/opt/central_monitor/txt/log
 LOG=/opt/central_monitor/txt/gpu_metrics.prom
 
-nvidia-smi > ${FILE} 
-
-TIME=`head -n1 ${FILE}`
-VERSION=`head -n3 ${FILE} | tail -n1 | cut -d' ' -f3`
 
 # Parser
 function gpu_parser {
-	rm -f ${LOG}
-	for LINE in 9 12 15 18 21 24 27 30
+	rm -f ${LOG} ${FILE}
+	for LINE in 0 1 2 3 4 5 6 7
 	do
-		GPU=`head -n${LINE}  ${FILE} | tail -n2`
-		GPU_NO=`echo ${GPU} | cut -d'|' -f2 | cut -d' ' -f2`
-		GPU_FAN=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f2 | sed 's/.$//'`
-		GPU_TEMP=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f3 | sed 's/.$//'`
-		GPU_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f5 | sed 's/.$//'`
-		GPU_MAX_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f7 | sed 's/.$//'`
-		GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2 | sed 's/...$//'`
-		GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4 | sed 's/...$//'`
-		GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2 | sed 's/.$//'`
-		
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_FAN\"} ${GPU_FAN}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_TEMP\"} ${GPU_TEMP}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MEM\"} ${GPU_MEM}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_MEM\"} ${GPU_MAX_MEM}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_PWR\"} ${GPU_PWR}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_PWR\"} ${GPU_MAX_PWR}" >> ${LOG}
-		echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_UTIL\"} ${GPU_UTIL}" >> ${LOG}
+		nvidia-smi -i ${LINE} > ${FILE} 
+
+		TIME=`head -n1 ${FILE}`
+		VERSION=`head -n3 ${FILE} | tail -n1 | cut -d' ' -f3`
+
+		echo ${TIME}
+		echo ${VERSION}
+
+		if [ "${VERSION}" != "384.111" ]; then
+			GPU_NO=${LINE}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_FAN\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_TEMP\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MEM\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_MEM\"} 6075" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_PWR\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_PWR\"} 120" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_UTIL\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_STATUS\"} 0" >> ${LOG}
+		else
+
+			GPU=`head -n9  ${FILE} | tail -n2`
+			GPU_NO=${LINE}
+			GPU_FAN=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f2 | sed 's/.$//'`
+			GPU_TEMP=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f3 | sed 's/.$//'`
+			GPU_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f5 | sed 's/.$//'`
+			GPU_MAX_PWR=`echo ${GPU} | cut -d'|' -f6 | cut -d' ' -f7 | sed 's/.$//'`
+			GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2 | sed 's/...$//'`
+			GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4 | sed 's/...$//'`
+			GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2 | sed 's/.$//'`
+			
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_FAN\"} ${GPU_FAN}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_TEMP\"} ${GPU_TEMP}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MEM\"} ${GPU_MEM}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_MEM\"} ${GPU_MAX_MEM}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_PWR\"} ${GPU_PWR}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_MAX_PWR\"} ${GPU_MAX_PWR}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_UTIL\"} ${GPU_UTIL}" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", content=\"GPU_STATUS\"} 1" >> ${LOG}
+
+		fi
 	done
 }
 
-echo ${TIME}
-echo ${VERSION}
 gpu_parser
