88f4f63e38a5218469e628469d14f5d4aecf2e47
Jackie <jackie@ghtinc.com>
2018-03-27 01:00:30 +0800

    batch_import.sh change minerID & title, change UI/miner_status format

--- script/batch_import.sh
+++ script/batch_import.sh
@@ -4,12 +4,13 @@ URL="localhost:3000/api/dashboards/db"
 USER="admin"
 PASS="admin"
 JSON="Content-type: application/json"
+NUM=`grep -c miner /etc/hosts`
 
 PAGEONE=`cat ../conf/grafana_dashboard_template/curl/page_one.json `
 curl -s -X POST ${URL} -u ${USER}:${PASS} -H "${JSON}" -d "${PAGEONE}" | python -m json.tool
 
-for ID in $(seq 1 3);
+for ID in $(seq 1 ${NUM});
 do
-	MINER=`cat ../conf/grafana_dashboard_template/curl/miner_status.json | sed "s/Miner 1/Miner ${ID}/g"`
+	MINER=`cat ../conf/grafana_dashboard_template/curl/miner_status.json | sed "s/Miner 1/Miner ${ID}/g; s/miner1/miner${ID}/g"`
 	curl -s -X POST ${URL} -u ${USER}:${PASS} -H "${JSON}" -d "${MINER}" | python -m json.tool
 done
