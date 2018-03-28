4c9ff23df819a6155d040edc884586179c90facb
ryan <dunkyfool0113@gmail.com>
2018-03-29 00:54:05 +0800

    fix MHS \r bug

--- script/gpu_scrap.sh
+++ script/gpu_scrap.sh
@@ -9,13 +9,13 @@ ETH=/opt/central_monitor/txt/gpu.log
 function gpu_parser {
 	rm -f ${LOG} ${FILE}
 
-	IP=`ifconfig | grep -A1 enp6s0 | grep -o inet.*Bcast | cut -d' ' -f2 | tr -d 'addr:'`
+	IP=`/sbin/ifconfig | grep -A1 enp6s0 | grep -o inet.*Bcast | cut -d' ' -f2 | tr -d 'addr:'`
 	MHS=`tac ${ETH} | head -n5 | grep ETH -m1 | cut -c26- | tr -d 'GPUMh/s' | tr -s ' '`
 	REBOOT=`last reboot | grep ^reboot | tr -s ' ' | awk -F' ' '{print $6 $7}' | sort | uniq -c | grep $(LANG=en_us_88591 date +%b%d) | tr -s ' ' | cut -d ' ' -f2`
 	if [ "${REBOOT}" = "" ]; then
 		REBOOT=0
 	fi
-	echo "node_gpu_metrics{ip=\"${IP}\", content=\"DAILY_REBOOT_COUNT\"} ${REBOOT}" >> ${LOG}
+	echo "node_gpu_metrics{devid=\"99\", ip=\"${IP}\", content=\"DAILY_REBOOT_COUNT\"} ${REBOOT}" >> ${LOG}
 
 	for LINE in 0 1 2 3 4 5 6 7
 	do
@@ -51,7 +51,7 @@ function gpu_parser {
 			GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2 | sed 's/...$//'`
 			GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4 | sed 's/...$//'`
 			GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2 | sed 's/.$//'`
-			GPU_MHS=`echo ${MHS} | cut -d ',' -f $((LINE+1)) | cut -c4-`
+			GPU_MHS=`echo ${MHS} | cut -d ',' -f $((LINE+1)) | cut -c4- | tr -d " \r"`
 
 			if [ "${GPU_MHS}" = "" ];then
 				GPU_MHS=0
