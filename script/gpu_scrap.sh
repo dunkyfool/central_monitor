fc1a0839953da543d884ec7290e5e243e5b9e22f
ryan <dunkyfool0113@gmail.com>
2018-03-29 00:01:44 +0800

    update mhs

--- script/gpu_scrap.sh
+++ script/gpu_scrap.sh
@@ -3,7 +3,7 @@
 # Declare
 FILE=/opt/central_monitor/txt/log
 LOG=/opt/central_monitor/txt/gpu_metrics.prom
-
+ETH=/opt/central_monitor/txt/gpu.log
 
 # Parser
 function gpu_parser {
@@ -15,10 +15,12 @@ function gpu_parser {
 		TIME=`head -n1 ${FILE}`
 		VERSION=`head -n3 ${FILE} | tail -n1 | cut -d' ' -f3`
 		IP=`ifconfig | grep -A1 enp6s0 | grep -o inet.*Bcast | cut -d' ' -f2 | tr -d 'addr:'`
+		MHS=`tac ${ETH} | head -n5 | grep ETH -m1 | cut -c26- | tr -d 'GPUMh/s' | tr -s ' '`
 
 		echo ${TIME}
 		echo ${VERSION}
 		echo ${IP}
+		echo ${MHS}
 
 		if [ "${VERSION}" != "384.111" ]; then
 			GPU_NO=${LINE}
@@ -30,6 +32,7 @@ function gpu_parser {
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_MAX_PWR\"} 120" >> ${LOG}
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_UTIL\"} 0" >> ${LOG}
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_STATUS\"} 0" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_MHS\"} 0" >> ${LOG}
 		else
 
 			GPU=`head -n9  ${FILE} | tail -n2`
@@ -41,6 +44,11 @@ function gpu_parser {
 			GPU_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f2 | sed 's/...$//'`
 			GPU_MAX_MEM=`echo ${GPU} | cut -d'|' -f7 | cut -d' ' -f4 | sed 's/...$//'`
 			GPU_UTIL=`echo ${GPU} | cut -d '|' -f8 | cut -d' ' -f2 | sed 's/.$//'`
+			GPU_MHS=`echo ${MHS} | cut -d ',' -f $((LINE+1)) | cut -c4-`
+
+			if [ "${GPU_MHS}" = "" ];then
+				GPU_MHS=0
+			fi
 			
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_FAN\"} ${GPU_FAN}" >> ${LOG}
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_TEMP\"} ${GPU_TEMP}" >> ${LOG}
@@ -50,6 +58,7 @@ function gpu_parser {
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_MAX_PWR\"} ${GPU_MAX_PWR}" >> ${LOG}
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_UTIL\"} ${GPU_UTIL}" >> ${LOG}
 			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_STATUS\"} 1" >> ${LOG}
+			echo "node_gpu_metrics{devid=\"${GPU_NO}\", ip=\"${IP}\", content=\"GPU_MHS\"} ${GPU_MHS}" >> ${LOG}
 
 		fi
 	done
