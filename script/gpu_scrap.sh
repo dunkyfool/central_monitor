a46eb9ffcb259c68c8a85dc4a3240cb9d5641623
ryan <dunkyfool0113@gmail.com>
2018-03-29 00:25:01 +0800

    daily reboot count

--- script/gpu_scrap.sh
+++ script/gpu_scrap.sh
@@ -8,14 +8,21 @@ ETH=/opt/central_monitor/txt/gpu.log
 # Parser
 function gpu_parser {
 	rm -f ${LOG} ${FILE}
+
+	IP=`ifconfig | grep -A1 enp6s0 | grep -o inet.*Bcast | cut -d' ' -f2 | tr -d 'addr:'`
+	MHS=`tac ${ETH} | head -n5 | grep ETH -m1 | cut -c26- | tr -d 'GPUMh/s' | tr -s ' '`
+	REBOOT=`last reboot | grep ^reboot | tr -s ' ' | awk -F' ' '{print $6 $7}' | sort | uniq -c | grep $(LANG=en_us_88591 date +%b%d) | tr -s ' ' | cut -d ' ' -f2`
+	if [ "${REBOOT}" = "" ]; then
+		REBOOT=0
+	fi
+	echo "node_gpu_metrics{ip=\"${IP}\", content=\"DAILY_REBOOT_COUNT\"} ${REBOOT}" >> ${LOG}
+
 	for LINE in 0 1 2 3 4 5 6 7
 	do
 		nvidia-smi -i ${LINE} > ${FILE} 
 
 		TIME=`head -n1 ${FILE}`
 		VERSION=`head -n3 ${FILE} | tail -n1 | cut -d' ' -f3`
-		IP=`ifconfig | grep -A1 enp6s0 | grep -o inet.*Bcast | cut -d' ' -f2 | tr -d 'addr:'`
-		MHS=`tac ${ETH} | head -n5 | grep ETH -m1 | cut -c26- | tr -d 'GPUMh/s' | tr -s ' '`
 
 		echo ${TIME}
 		echo ${VERSION}
