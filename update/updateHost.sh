ae7db23558d2c843c52335a82bbae13b20178d74
root <dunkyfool0113@gmail.com>
2018-04-08 11:52:17 +0800

    update host

--- update/updateHost.sh
+++ update/updateHost.sh
@@ -3,13 +3,13 @@
 PASS=digitalespacio
 IP=192.168.88.215
 
-echo ${PASS} | sudo -E cp /etc/hosts /tmp/hosts
+echo ${PASS} | sudo -E cp /etc/hosts hosts
 
-if `grep -q central_monitor /tmp/hosts`
+if `grep -q central_monitor hosts`
 then 
-   sed -i "s/$(grep central /tmp/hosts)/${IP} central_monitor/g" /tmp/hosts
+   sed -i "s/$(grep central hosts)/${IP} central_monitor/g" hosts
 else
-   echo "${IP} central_monitor" > /tmp/hosts
+   echo "${IP} central_monitor" > hosts
 fi
 
-echo ${PASS} | sudo -E cp /tmp/hosts /etc/hosts
+echo ${PASS} | sudo -E cp hosts /etc/hosts
