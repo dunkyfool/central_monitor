5f0837d9a77fb3127889d72b93a059a75881cd12
ryan <dunkyfool0113@gmail.com>
2018-04-08 16:02:03 +0800

    fix permission denied

--- update/updateHost.sh
+++ update/updateHost.sh
@@ -4,12 +4,13 @@ PASS=digitalespacio
 IP=192.168.88.215
 
 echo ${PASS} | sudo -E cp /etc/hosts hosts
+echo ${PASS} | sudo -E chown ethos:ethos hosts
 
 if `grep -q central_monitor hosts`
 then 
    sed -i "s/$(grep central hosts)/${IP} central_monitor/g" hosts
 else
-   echo "${IP} central_monitor" > hosts
+   echo "${IP} central_monitor" >> hosts
 fi
 
 echo ${PASS} | sudo -E cp hosts /etc/hosts
